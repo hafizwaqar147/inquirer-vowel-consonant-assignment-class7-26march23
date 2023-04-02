bc94ce58e105e46401b679ff06486a218adf2638
Hafiz M.Waqar <126373803+hafizwaqar147@users.noreply.github.com>
2023-04-03 00:18:33 +0500

    Add files via upload

---
+++ calculator1.ts
@@ -0,0 +1,40 @@
+import inquirer from "inquirer";
+
+const calculator1 = await inquirer.prompt([
+    {
+        name: "first_number",
+        type: "number",
+        message: "Enter the First Number"
+    },{
+        name: "second_number",
+        type: "number",
+        message: "Enter the Second Number"
+    }
+]);
+
+const calculator2 = await inquirer.prompt([
+    {
+        name: "operator",
+        type: "list",
+        message: "Select the operator",
+        choices: ["+", "-", "*", "/", "%"]
+    }
+]);
+
+let {first_number, second_number} = calculator1;
+let {operator} = calculator2;
+let result: number;
+
+if(operator === "+"){
+    console.log("Result: ",result = first_number + second_number);
+}else if(operator === "-"){
+    console.log("Result: ",result = first_number - second_number);
+}else if(operator === "*"){
+    console.log("Result: ",result = first_number * second_number);
+}else if(operator === "/"){
+    console.log("Result: ",result = first_number / second_number);
+}else if(operator === "%"){
+    console.log("Result: ",result = first_number % second_number);
+}else{
+    console.log("Invalid Operator");
+};
